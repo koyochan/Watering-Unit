0038b747f1e37ac678d5a671fa1e76b1e0898c04
koyochan <121277745+koyochan@users.noreply.github.com>
2024-09-27 22:29:03 +0900

    add

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "../lib/enable.h"
-#include "../lib/secrets.h"
 #include "../lib/watering_unit.h"
 
 float vol, moisture;
@@ -17,11 +16,11 @@ void	loop(void)
 	M5.update();
 	measureAndDisplayMoisture(vol, moisture);
 	controlPump(moisture, number);
-#if ENABLE_GRAPH_DISPLAY
-	void displayGraph(float moisture, float moistureHistory[],
-		int currentIndex);
-#endif
-#if ENABLE_LINE_NOTIFY
-	void notifyLine(float vol, float moisture);
-#endif
+	#if ENABLE_GRAPH_DISPLAY
+		void displayGraph(float moisture, float moistureHistory[],
+			int currentIndex);
+	#endif
+	#if ENABLE_LINE_NOTIFY
+		void notifyLine(float vol, float moisture);
+	#endif
 }
